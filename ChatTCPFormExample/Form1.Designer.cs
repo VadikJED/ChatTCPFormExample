615d503428edbea1359815cc8c4ce926ee5da34d
ВАДИК <VIVANOV@newlaser.ru>
2021-02-12 13:56:15 +0300

    Первый

--- ChatTCPFormExample/Form1.Designer.cs
+++ ChatTCPFormExample/Form1.Designer.cs
@@ -29,13 +29,62 @@ namespace ChatTCPFormExample
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 58);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(588, 272);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(613, 342);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- ChatTCPFormExample/Form1.cs
+++ ChatTCPFormExample/Form1.cs
@@ -7,14 +7,207 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+
 
 namespace ChatTCPFormExample
 {
     public partial class Form1 : Form
     {
+        static string userName;
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
-    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Console.Write("Введите свое имя: ");
+            userName = "@#$";
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+
+                string message = userName;
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                // запускаем новый поток для получения данных
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start(); //старт потока
+               // Console.WriteLine("Добро пожаловать, {0}", userName);
+
+                textBox2.Text = ("Добро пожаловать, {0}", userName) + "\r\n" + textBox2.Text;
+                // SendMessage();
+            }
+            catch (Exception ex)
+            {
+
+                
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+               // Disconnect();
+            }
+        }
+
+
+
+
+
+
+
+
+        // отправка сообщений
+        void SendMessage(string message)
+        {
+            //Console.WriteLine("Введите сообщение: ");
+
+            //while (true)
+            //{
+               // string message = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                textBox2.Text = message + "\r\n" + textBox2.Text;
+
+            //}
+        }
+        // получение сообщений
+        void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[256]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    //Console.WriteLine(message);//вывод сообщения
+                    //textBox2.Text = message + "\r\n" + textBox2.Text;
+
+
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        //string time = DateTime.Now.ToShortTimeString();
+                        addData(/*time + " " +*/ message + "\r\n" + textBox2.Text);
+                    }));
+
+
+
+                    if (message == "PleaseTurnOffTheClient")
+                    {
+                       
+
+                        //textBox2.Text = message + " Подключение прервано!" + "\r\n" + textBox2.Text;
+
+                        Invoke(new MethodInvoker(() =>
+                        {
+                            addData(message + " Подключение прервано!" + "\r\n" + textBox2.Text);
+                        }));
+
+
+
+
+                        Disconnect();
+                        break;
+                    }
+
+
+
+
+
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!"); //соединение было прервано
+
+                    //Invoke(new MethodInvoker(() =>
+                    //{
+                    //    addData("Подключение прервано!" + "\r\n" + textBox2.Text);
+                    //}));
+
+
+                    //Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();//отключение потока
+            if (client != null)
+                client.Close();//отключение клиента
+           // Environment.Exit(0); //завершение процесса
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMessage(textBox1.Text);
+        }
+
+
+
+        object locker = new object();
+        public void addData(string data)
+        {
+            lock (locker)
+            {
+
+                try
+                {
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        string time = DateTime.Now.ToShortTimeString();
+                        textBox2.Text = time + " " + data + "\r\n" + textBox2.Text;
+                    }));
+
+                }
+                catch
+                {
+                    //Form1.allDead = false;
+
+                    ////if (server != null)
+                    //server.DisconnectListener();
+                }
+
+
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+        }
 }
